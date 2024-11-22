7b50258c17d5f9cd96fabd265a03109df950e49c
glowseoni <glowseoni@gmail.com>
2024-11-22 14:42:46 +0900

    생활정보 퍼블리싱 수정

--- frontend/packages/map2d/src/components/subcontent/life/LifeTrafficAccidentArea.tsx
+++ frontend/packages/map2d/src/components/subcontent/life/LifeTrafficAccidentArea.tsx
@@ -17,9 +17,11 @@ export const LifeTrafficAccidentArea = ({ analysisConditions }: LifeTrafficAccid
     // 분석조건 상태
     const [areaType, setAreaType] = useState<string>(analysisConditions.areaType);
     const [weather, setWeather] = useState<boolean>(false);
+    const [dataPopup, setDataPopup] = useState<boolean>(false);
 
     const handleAreaTypeChange = (e: React.ChangeEvent<HTMLInputElement>) => setAreaType(e.target.value); 
     const handleWeatherChange = () => setWeather(!weather);
+    const handleDataPopup = () => setDataPopup(!dataPopup);
     
     return(
         <div>
@@ -83,10 +85,50 @@ export const LifeTrafficAccidentArea = ({ analysisConditions }: LifeTrafficAccid
                 </div>
             </div>
             <div className="button-large-wrapper">
+                <button type="button" className={dataPopup ? 'normal-button apply' : 'normal-button cancel'} style={{height: '40px'}} onClick={handleDataPopup}>제공데이터 확인</button>
                 <button type="button" className="large-button apply">
                     <span className="txt">조회</span>
                 </button>
             </div>
+            {dataPopup && (
+                <div className="popup popup-parcel" style={{left: '400px', top: '0'}}>
+                    <button type="button" className="close popup-close"></button>
+                    <h2><span className="txt">제공데이터</span></h2>
+                    <div className="content-wrapper mar-top-20">
+                    <div className="explanation">도로교통공단 교통사고분석시스템에서 제공된 사망교통사고, 보행자무단횡단사고 다발지역 등을 통하여 사고 다발지역을 제공하는 서비스입니다.</div>
+                    <table className="basic-2col mar-top-7">
+                        <tr>
+                            <th>데이터</th>
+                            <th>출처</th>
+                        </tr>
+                        <tr>
+                            <td>사망교통사고 다발지역</td>
+                            <td>교통사고분석시스템 <br/> 2012년 ~ 2022년</td>
+                        </tr>
+                        <tr>
+                            <td>자전거사고 다발지역</td>
+                            <td>교통사고분석시스템 <br/> 2013년 ~ 2021년</td>
+                        </tr>
+                        <tr>
+                            <td>스쿨존내어린이사고</td>
+                            <td>교통사고분석시스템 <br/> 2013년 ~ 2022년</td>
+                        </tr>
+                        <tr>
+                            <td>보행자무단횡단사고 다발지역</td>
+                            <td>교통사고분석시스템 <br/> 2013년 ~ 2021년</td>
+                        </tr>
+                        <tr>
+                            <td>보행어린이사고 다발지역</td>
+                            <td>교통사고분석시스템 <br/> 2013년 ~ 2023년</td>
+                        </tr>
+                        <tr>
+                            <td>보행노인사고 다발지역</td>
+                            <td>교통사고분석시스템 <br/> 2013년 ~ 202년</td>
+                        </tr>
+                    </table>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
